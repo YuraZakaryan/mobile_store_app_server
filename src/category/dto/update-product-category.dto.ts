69394cd68294c9e27c70d0ae8650172c19a3db64
Yura Zakaryan <yura.zakaryan@braint.systems>
2024-08-16 09:51:01 +0400

    removed length validator from category

--- src/category/dto/update-product-category.dto.ts
+++ src/category/dto/update-product-category.dto.ts
@@ -1,12 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateProductCategoryDto {
   @ApiProperty({ example: 'USB', description: 'keyword' })
   @IsString({ message: 'keyword - must be a string' })
   @IsNotEmpty({ message: 'keyword - is required' })
-  @Length(3, 12, {
-    message: 'keyword - length should be between 2 and 48 characters',
-  })
   readonly keyword: string;
 }
